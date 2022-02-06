c0f5a4029780c8cb5943b2afc6e79fc47851f103
Bryan Xu <bryanbobo123@gmail.com>
2022-02-05 18:49:19 -0800

    bx - implemented chat funcionality

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,21 +1,36 @@
-import React from'react';
+import React, { useState } from'react';
 
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql,
+    useMutation,
+    useSubscription,
 } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import {
+    Container,  Row, Col, FormInput, Button
+} from "shards-react"
+
+const link = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+      reconnect: true
+    }
+  });
+
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
+    link,
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
 });
 
 
 const GET_MESSAGES = gql`
-query {
+subscription {
     messages {
         id
         content
@@ -24,18 +39,121 @@ query {
 }
 `;
 
+const POST_MESSAGE = gql`
+mutation ($user:String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+}
+`
+
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES);
+    const { data } = useSubscription(GET_MESSAGES);
     console.log("inside messages", data);
     if (!data) {
-        return  "No Messages";
+        return  null;
     }
-    return JSON.stringify(data);
+    return (
+        <>
+            {data.messages.map(({id, user: messageUser, content}) => (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: user === messageUser? 'flex-end' : 'flex-start',
+                        paddingBottom: '1em',
+                    }}
+                >
+                    {user !== messageUser && (
+                        <div
+                            style={{
+                                height: 50,
+                                width: 50,
+                                marginRight: '0.5em',
+                                border: '2px solid #e5e6ea',
+                                borderRadius: 25,
+                                textAlign: 'center',
+                                fontSize: '18pt',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            {messageUser.slice(0,2).toUpperCase()}
+                        </div>
+                    )}
+                    <div
+                        style={{
+                            background: user === messageUser ? '#58bf56' : '#e5e6ea',
+                            color: user === messageUser ? 'white' : 'black',
+                            padding: '1em',
+                            borderRadius: '1em',
+                            maxWidth: '60%',
+                        }}
+                    >
+                        {content}
+                    </div>
+                </div>
+            ))} 
+        </>
+    );
 }
 
 const Chat = () => {
+    const [state, setState] = useState({
+        user: 'Bryan',
+        content: '',
+    })
+
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () => {
+        if (state.content.length > 0) {
+            postMessage({
+                variables: state,
+            });
+        }
+
+        setState({
+            ...state,
+            content: "",
+        });
+    }
+
     return (
-        <div><Messages user="Bryan" /></div>
+        <Container>
+            <Messages user={state.user} />
+            <Row>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <FormInput 
+                        label = "User"
+                        value={state.user}
+                        onChange={(evt) => setState({
+                            // TODO: CHANGE LATER FOR USER BASE
+                            ...state, 
+                            user: evt.target.value,
+                        })}
+                    />
+                </Col>
+                <Col xs={8}>
+                    <FormInput 
+                        label = "Content"
+                        value={state.content}
+                        onChange={(evt) => setState({
+                            ...state, 
+                            content: evt.target.value,
+                        })}
+                        onKeyUp={(evt) => {
+                            if (evt.keyCode === 13) {
+                                onSend();
+                            }
+                        }}
+                    />
+                </Col>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <Button onClick={() => onSend()}>
+                        Send
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
